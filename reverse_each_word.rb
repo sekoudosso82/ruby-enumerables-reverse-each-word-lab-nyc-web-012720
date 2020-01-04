17c25a8676834e8e2c77d9c1f643fa24b81cf85e
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-04 16:35:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,7 @@
 def reverse_each_word(arg)
   # split the token 
   array = arg.split() 
+  # collect each element and reverse it 
   array1 = array.collect {|element| element.reverse }
   array1.join(" ")
 end 
-
-# def reverse_string str
-#   arr = str.split /\b/
-#   new_arr = arr.collect {|a| a.reverse}
-#   new_arr.join
-# end
