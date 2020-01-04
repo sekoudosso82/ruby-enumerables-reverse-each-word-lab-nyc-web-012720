f80189efd3b6568ef7525fc0e069b38eb03c9462
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-04 16:30:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
-filter_parameter_logging
+def reverse_each_word(arg)
+  # split the token 
+  array = arg.split() 
+  array1 = array.collect {|element| element.reverse }
+  # array1.join
+end 
 
 # def reverse_string str
 #   arr = str.split /\b/
