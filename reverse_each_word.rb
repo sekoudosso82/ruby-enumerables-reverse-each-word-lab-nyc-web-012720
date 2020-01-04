4a134a3bbd8a703729cd8d39a05d260e20b5976e
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-04 16:30:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
-def reverse_each_word(arg)
-  # split the token 
-  array = arg.split() 
-  array1 = array.collect {|element| element.reverse }
-  # array1.join
-end 
+filter_parameter_logging
 
 # def reverse_string str
 #   arr = str.split /\b/
